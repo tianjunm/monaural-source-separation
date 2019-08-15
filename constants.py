fixed loss computation and string issues

constants.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 ROOT_DIR = "/home/ubuntu/"
-TRAIN_SCALE = 1e4
+TRAIN_SCALE = 10000
 DATASET_PATH_PREFIX = os.path.join(ROOT_DIR, "datasets/processed/mixer/")
 RESULT_PATH_PREFIX = os.path.join(ROOT_DIR, "/home/ubuntu/experiment_logs/results")
 DATASET_PATH = '/home/ubuntu/datasets/processed/datagen/'
@@ -20,9 +20,9 @@ LOG_FREQ = 10
 CHECKPOINT_FREQ = 1
 
 MAX_LOSS = np.inf
-UM_CONFIGS = 10
+NUM_CONFIGS = 10
 NUM_TRIALS = 1
-TOLERANCE = 1000  # for early stopping
+TOLERANCE = 50 # for early stopping
 
 FIELD_NAMES = [
     'id',

model/dataset.py
@@ -31,8 +31,7 @@ def get_spect(wav, ttype):
         wav_arr,
         WINDOW_SIZE,
         HOP_LENGTH,
-        window=torch.hann_window(256),
-        normalized=True).permute(2, 1, 0)
+        window=torch.hann_window(256)).permute(2, 1, 0)
 
     if ttype == 'Concat':
         return torch.cat([spect[0], spect[1]], dim=-1)
@@ -86,7 +85,7 @@ class MixtureDataset(Dataset):
         # load original files according to filenames
         filename = instances.at[iid, 'filename']
         dur = instances.at[iid, 'clip_duration']
-        start = instances.at[iid, 'start_time']
+        start = instances.at[iid, 'mixture_placement']
         # start, end = parse_range(instances.at[iid, 'mixture_placement'])
         # print(filename)
 

model/models.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
-from tensorboardX import SummaryWriter
 import scipy.spatial.distance as ssdist
 
 
@@ -50,7 +49,7 @@ def get_matches(orders):
             orders[batch, indices[0], :] = np.ones(d) * mask
             orders[batch, :, indices[1]] = np.ones(d) * mask
 
-    return matched_pairs
+    return list(matched_pairs.astype(int))
 
 
 def reshape(x, seq_len, bs):
@@ -89,7 +88,7 @@ def get_correlation(m1, m2):
     return -(1 - n / d)
 
 
-def get_min_dist(preds, gts, device, metric):
+def get_loss(preds, gts, device, metric):
     """
     Args:
         preds(tensor): [bs, seq_len, n_sources, input_dim]
@@ -98,7 +97,7 @@ def get_min_dist(preds, gts, device, metric):
     Returns:
         dists: [bs, n_sources]
     """
-    bs, _, n_sources, _ = preds.size()
+    bs, seq_len, n_sources, input_dim = preds.size()
 
     # getting the distances from each prediction to all gts
     all_dists = get_dists(preds, gts, bs, n_sources, metric)
@@ -115,11 +114,18 @@ def get_min_dist(preds, gts, device, metric):
             if metric == 'correlation':
                 dist = get_correlation(pred, gt_match)
             else:
-                dist = torch.norm(pred - gt_match)
+                dist = torch.norm((pred - gt_match) / (seq_len * input_dim))
 
             dists[b, src_id] = dist
 
+    # for b in range(bs):
+    #     gts[b] = gts[b, :, all_matches[b]]
+
+    # mse = nn.MSELoss()
+    # loss = mse(preds, gts)
     return dists
+
+    # return loss
 
 
 class DiscrimLoss(nn.Module):
@@ -147,15 +153,17 @@ class DiscrimLoss(nn.Module):
 
         penalties = self.gamma * \
             self._calc_dists(pred_swapped, ground_truths)
-        sum_ = torch.sum(dists, 1) ** 2 + torch.sum(penalties, 1) ** 2
+        # sum_ = torch.sum(dists, 1) ** 2 + torch.sum(penalties, 1) ** 2
 
         # loss = torch.sum(dists)
-        loss = torch.log(torch.sqrt(sum_.mean()))
+        # loss = torch.log(torch.sqrt(sum_.mean()))
 
-        return loss
+        return dists.mean() + penalties.mean()
+
+        # return mse(predictions, ground_truths) + penalty
 
     def _calc_dists(self, preds, gts):
-        bs, _, n_sources, _ = preds.size()
+        bs, seq_len, n_sources, input_dim = preds.size()
         dists = torch.zeros(bs, n_sources).to(self.device)
 
         for b in range(bs):
@@ -165,7 +173,7 @@ class DiscrimLoss(nn.Module):
                 if self.metric == 'correlation':
                     dist = get_correlation(pred[b], gt_match[b])
                 else:
-                    dist = torch.norm(pred[b] - gt_match[b])
+                    dist = torch.norm((pred[b] - gt_match[b]) / (seq_len * input_dim))
                 dists[b, src_id] = dist
         return dists
 
@@ -176,12 +184,11 @@ class GreedyLoss(nn.Module):
     Compare the distance from output with its closest ground truth.
 
     """
-    def __init__(self, device, metric, gamma, num_sources):
+    def __init__(self, device, metric, num_sources):
         # nn.Module.__init__(self)
-        super(GreedyLoss, self).__init__()
+        super().__init__()
         self.device = device
         self.metric = metric
-        self.gamma = gamma
         self.num_sources = num_sources
 
     def forward(self, predictions, ground_truths):
@@ -193,19 +200,10 @@ class GreedyLoss(nn.Module):
             loss: [bs,]
         """
 
-        # get distance measure (bs * num_sources)
-        dists = get_min_dist(
-            predictions,
-            ground_truths,
-            self.device,
-            self.metric)
+        # dists = get_loss
+        dists = get_loss(predictions, ground_truths, self.device, self.metric)
 
-        # losses across instances, normalized wrt nsrc
-        avg_dists = dists.mean(dim=1)
-
-        # MSE
-        loss = (avg_dists ** 2).mean()
-
+        loss = dists.mean()
         return loss
 
 

model/transformer.py
@@ -102,12 +102,14 @@ def make_detf(
 
     position = PositionalEncoding(d_model, dropout)
     # FIXME: experimenting with num_sources
-    model = EncoderDecoder(
-        DoubleEncoder(
-            EncoderLayer(d_model, c(attn), c(ff), dropout),
-            EncoderLayer(seq_len, c(attn_t), c(ff_t), dropout), N),
+    model = DoubleEncoderDecoder(
+        # DoubleEncoder(
+        Encoder(EncoderLayer(d_model, c(attn), c(ff), dropout), N),
+        Encoder(EncoderLayer(seq_len, c(attn_t), c(ff_t), dropout), N),
+            # N),
         Decoder(
-            DecoderLayer(d_model, c(attn), c(attn), c(ff), dropout), N),
+            DecoderLayer(d_model, c(attn), c(attn), c(ff), dropout),
+            N),
         nn.Sequential(Embeddings(d_model, input_dim), c(position)),
         nn.Sequential(
             Embeddings(d_model, input_dim, num_sources=num_sources),
@@ -385,7 +387,7 @@ class Encoder(nn.Module):
 class DoubleEncoder(nn.Module):
     "Core encoder is a stack of N layers"
     def __init__(self, layer, layer_t, N):
-        super(DEncoder, self).__init__()
+        super(DoubleEncoder, self).__init__()
         self.layers = clones(layer, N)
         self.layer_ts = clones(layer_t, N)
         self.norm = LayerNorm(layer.size)

train.py
@@ -9,6 +9,7 @@ import torch.optim
 import utils
 import constants as const
 
+# FIXME: module rename
 import model.dataset as custom_dataset
 import model.models as custom_models
 import model.transformer as custom_transformer
@@ -22,7 +23,7 @@ logging.basicConfig(format="[%(levelname)s] %(message)s", level=logging.INFO)
 
 class Trainer():
     """Trainer object that runs through a single experiment"""
-    def __init__(self, setup, config, experiment_path, has_checkpoint=False):
+    def __init__(self, setup, config, experiment_path, load_checkpoint=False):
 
         self._device = setup['device']
         self._ds_type = setup['dataset_type']
@@ -35,7 +36,7 @@ class Trainer():
 
         self._config = config
         self._epath = experiment_path
-        self._cp = has_checkpoint
+        self._cp = load_checkpoint
 
         self._ds_size = self._nsrc * const.TRAIN_SCALE
         # self.tb_writer = tb_writer
@@ -137,6 +138,7 @@ class Trainer():
             content['metric'] = self._metric
             content['stop_epoch'] = epoch
             content['best_val_loss'] = best_val_loss
+            content['experiment_path'] = self._epath
             csv_writer.writerow(content)
 
     def train(self, epoch):
@@ -153,7 +155,7 @@ class Trainer():
 
             if (batch_idx + 1) % log_size == 0:
                 logging.info(
-                    "[%2d/%2d] %s, epoch %3d, [%5d/%5d] "
+                    "[%2d/%2d] %s: epoch %3d, [%5d/%5d] "
                     "train loss: %.2f",
                     self._config['id'] + 1,
                     self._nconf,
@@ -226,10 +228,10 @@ class Trainer():
 
         # e.g. results/setup_name/config_0/snapshots/checkpoint.tar
         # e.g. results/setup_name/config_0/snapshots/best.tar
-        model_path= os.path.join(
+        model_path = os.path.join(
             const.RESULT_PATH_PREFIX,
             self._epath,
-            self._config['id'],
+            str(self._config['id']),
             "snapshots",
             model_name)
 
@@ -256,7 +258,6 @@ class Trainer():
             criterion = custom_models.GreedyLoss(
                 self._device,
                 self._metric,
-                gamma,
                 self._nsrc)
 
         elif self._config['loss_fn'] == "Min":
@@ -308,7 +309,7 @@ class Trainer():
 
             data_path = os.path.join(
                 const.DATASET_PATH,
-                self._epath.split['_'][0],
+                self._epath.split('_')[0],
                 f"{ds_name}.csv")
 
             dataset = custom_dataset.MixtureDataset(
@@ -368,7 +369,6 @@ class Trainer():
 
         elif self._m_type == "SRNN":
             self._config['loss_fn'] = 'Discrim'
-            # self._config['optim'] = 'LBFGS'
 
             transform = custom_dataset.Wav2Spect('Concat')
             model = huang_srnn.SRNN(
@@ -408,8 +408,9 @@ class Trainer():
         logging.info("loading checkpoint from configuration #%d...", config_id)
 
         checkpoint_path = os.path.join(
+            const.RESULT_PATH_PREFIX,
             self._epath,
-            config_id,
+            str(config_id),
             "snapshots",
             "checkpoint.tar")
 